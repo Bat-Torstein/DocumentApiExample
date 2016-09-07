688bcabab184f93952db1e9505664fec5a6f39a1
Torstein Bjørgan <tob@itema.no>
2016-09-07 22:06:32 +0200

    Step 1: Introduce angular2.

---
+++ client/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'testApp',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent { } 
